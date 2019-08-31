d67e4325263c8f609e4fe202acb25f4c7deb9ca6
League Student <league@iMac-27.attlocal.net>
2019-08-31 15:56:58 -0700

    Crazy Digital Painting, Pixel Art, Conways game of life(IP)

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,13 +1,17 @@
 package _01_Crazy_Digital_Painting;
 
+import java.awt.Color;
+
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
+	final static int width = 1000;
+	final static int height = 1038;
 
 	
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
-	
+	Color [][] a = new Color[1000][1038];
 	
 	
 	public CrazyDigitalPainting() {
@@ -17,11 +21,17 @@ public class CrazyDigitalPainting {
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
+		for(int i = 0; i < a.length; i++) {
+			for(int b = 0; b < a[i].length; b++) {
+			a[i][b] = new Color(i % 20, (i * b) % 100, b % 150);
+			}
+		}
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
 		//   to show off your picture.
+		ColorArrayDisplayer c = new ColorArrayDisplayer();
+		c.displayColorsAsImage(a);
 	}
 	
 	public static void main(String[] args) {
